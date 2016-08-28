67836eb622c2cac237c18fb0f4cb493195cd1f0d
João Grego <jpgrego@gmail.com>
2016-08-28 22:51:21 +0100

    Forgotten files

---
+++ app/src/main/java/com/jpgrego/thesisapp/thesisapp/utils/WifiInfoReceiver.java
@@ -0,0 +1,68 @@
+package com.jpgrego.thesisapp.thesisapp.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+import com.jpgrego.thesisapp.thesisapp.data.WifiAP;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by jpgrego on 24-08-2016.
+ */
+public class WifiInfoReceiver extends BroadcastReceiver {
+
+    private final Set<WifiAP> wifiAPSet = new HashSet<>();
+    private final WifiManager wifiManager;
+
+    public WifiInfoReceiver(final WifiManager wifiManager) {
+        this.wifiManager = wifiManager;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final List<ScanResult> wifiScanList;
+        WifiAP tempAP;
+
+        wifiScanList = wifiManager.getScanResults();
+
+
+        if (wifiScanList != null) {
+            synchronized (wifiAPSet) {
+                for (Iterator<WifiAP> it = wifiAPSet.iterator(); it.hasNext(); ) {
+                    tempAP = it.next();
+                    if (tempAP.getVisibilityCounter() > 0) {
+                        tempAP.decrementVisibilityCounter();
+                    } else if (tempAP.getVisibilityCounter() <= 0) {
+                        it.remove();
+                    }
+                }
+
+                for (ScanResult scanResult : wifiScanList) {
+                    tempAP = new WifiAP(scanResult);
+                    wifiAPSet.remove(tempAP);
+                    wifiAPSet.add(tempAP);
+                }
+            }
+        }
+
+    }
+
+    public List<WifiAP> getOrderedWifiAPList() {
+        final List<WifiAP> wifiAPList;
+
+        synchronized (wifiAPSet) {
+            wifiAPList = new ArrayList<>(wifiAPSet);
+            Collections.sort(wifiAPList);
+            return Collections.unmodifiableList(wifiAPList);
+        }
+    }
+}
